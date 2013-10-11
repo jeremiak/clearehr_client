5b0ce8876addbf84070873260e1929f3558190e9
Jeremia Kimelman <jbkimelman@gmail.com>
2013-10-11 16:11:34 -0700

    services rendered scaffold and basic test

---
+++ services_rendered.py
@@ -0,0 +1,34 @@
+from base import BaseObject, CreationFailedException
+
+class ServicesRendered(BaseObject):
+  optional_arguments = ['diagnosis2', 'diagnosis3', 'diagnosis4', 'diagnosis5',
+      'diagnosis6', 'diagnosis7', 'diagnosis8', 'diagnosis9', 'diagnosis10',
+      'diagnosis11', 'diagnosis12', 'last_menstrual', 'date_admission']
+
+  def __init__(self, client_total_payments=None,
+      diagnosis1=None,
+      place_of_service=None,
+      service_start_date=None,
+      service_end_date=None,
+      service_lines=None, **kwargs):
+
+    if None not in locals().values():
+      self.client_total_payments = client_total_payments
+      self.diagnosis1 = diagnosis1
+      self.place_of_service = place_of_service
+      self.service_start_date = service_start_date #YYYYMMDD
+      self.service_end_date = service_end_date #YYYYMMDD
+      self.service_lines = service_lines
+
+      for service_line in service_lines:
+        self.total_claim += service_line.line_item_amount
+    else:
+      raise CreationFailedException 
+
+    for k,v in kwargs.items():
+      if k in self.optional_arguments:
+        self.__setattr__(k, v)
+
+  def __str__(self):
+    return 'ServicesRendered: %s for %s' % ()
+
---
+++ tests/test_services_rendered.py
@@ -0,0 +1,41 @@
+from nose.tools import *
+
+from APIConnector.base import CreationFailedException
+from APIConnector.services_rendered import ServicesRendered 
+
+@raises(CreationFailedException)
+def test_create_services_rendered_with_no_options():
+  services_rendered = ServicesRendered() 
+
+def test_create_services_rendered_with_all_options():
+  services_rendered = ServicesRendered(client_total_payments='REQUIRED',
+      diagnosis1='REQUIRED',
+      place_of_service='REQUIRED',
+      service_start_date='REQUIRED',
+      service_end_date='REQUIRED',
+      service_lines=[])
+
+  assert services_rendered.__class__ == ServicesRendered 
+
+def test_create_services_rendered_with_optional_parameter():
+  services_rendered = ServicesRendered(client_total_payments='REQUIRED',
+      diagnosis1='REQUIRED',
+      place_of_service='REQUIRED',
+      service_start_date='REQUIRED',
+      service_end_date='REQUIRED',
+      service_lines=[],
+      diagnosis2='OPTIONAL') 
+
+  assert services_rendered.diagnosis2 == 'OPTIONAL'
+
+@raises(AttributeError)
+def test_create_services_rendered_with_invalid_optional_paramter():
+  services_rendered = ServicesRendered(client_total_payments='REQUIRED',
+      diagnosis1='REQUIRED',
+      place_of_service='REQUIRED',
+      service_start_date='REQUIRED',
+      service_end_date='REQUIRED',
+      service_lines=[],
+      invalid_parameter='INVALID')
+
+  services_rendered.invalid
